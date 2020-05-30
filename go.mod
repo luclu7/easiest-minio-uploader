0619e2b5d0862b7e13cd02ac7c875057e1d5809d
luclu7 <me@luclu7.fr>
2020-05-30 20:57:51 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luclu7/easiest-minio-uploader
+
+go 1.14
+
+require github.com/minio/minio-go/v6 v6.0.56
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/minio/minio-go/v6"
+	"log"
+	"os"
+)
+
+func main() {
+	endpoint := os.Getenv("S3_HOST")
+	accessKeyID := os.Getenv("S3_KEY")
+	secretAccessKey := os.Getenv("S3_SECRET")
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Make a new bucket called mymusic.
+	bucketName := os.Args[1]
+
+	err = minioClient.MakeBucket(bucketName, os.Getenv("S3_REGION"))
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	// Upload the zip file
+	objectName := os.Args[2]
+	filePath := os.Args[2]
+	contentType := "application/octet-stream"
+
+	// Upload the zip file with FPutObject
+	n, err := minioClient.FPutObject(bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
+}
