1fdfab401af186a11bb2e6325c6984ab21b97933
luclu7 <me@luclu7.fr>
2020-05-30 21:53:46 +0200

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Just a lazy, simple minio uploader
+
+Usage:
+./easiest-minio-uploader <bucketname> <file you want to upload> <name of the remote file>
+
+Env variables:
+* S3_HOST: address of you minio/s3 server
+* S3_KEY: your access key
+* S3_SECRET: your secret key
+* S3_REGION: your S3/minio region (defaults to us-east-1)
--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// Make a new bucket called mymusic.
 	bucketName := os.Args[1]
 
-	err = minioClient.MakeBucket(bucketName, os.Getenv("S3_REGION"))
+	err = minioClient.MakeBucket(bucketName, os.Getenv("S3_REGION", "us-east-1"))
 	if err != nil {
 		// Check to see if we already own this bucket (which happens if you run this twice)
 		exists, errBucketExists := minioClient.BucketExists(bucketName)
